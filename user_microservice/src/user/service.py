cec4d763665c306268135f6338953d66032e6e9a
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-11-19 16:36:11 +0700

    Обновлена структура

--- user_microservice/src/user/service.py
+++ user_microservice/src/user/service.py
@@ -1,17 +1,27 @@
 from dataclasses import dataclass
+from typing import Any, Sequence
 
 from fastapi import HTTPException, Depends
+from sqlalchemy import Row, RowMapping
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
 
-from src.user_microservice.settings.database import get_session
-from src.user_microservice.user.repository import UserRepository
-from src.user_microservice.user.schemas import CreateUserSchemas
+from src.settings.database import get_session
+from src.settings.exceptions import UserDontExistException
+from src.user.repository import UserRepository
+from src.user.schemas import CreateUserSchemas
 
 
 @dataclass
 class UserService(UserRepository):
     """Сервис пользователей"""
+
+    async def find_user_by_username(self, username: str) -> Sequence[Row[Any] | RowMapping | Any]:
+        """Поиск пользователя по username"""
+        user = await self._repository_find_user_by_username(username)
+        if not user:
+            raise UserDontExistException
+        return user
 
     async def create_user(self, schemas: CreateUserSchemas, request: Request) -> dict | HTTPException:
         """Создание пользователя"""
