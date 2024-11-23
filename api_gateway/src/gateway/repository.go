55185d0393964f7d8dc0c98c47aba6187cb14393
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-11-23 13:49:33 +0700

    Обновил функцию проверки токена
    1. Добавил проверку есть ли такой токен
    2. Добавил проверку на срок действия токена с последующим удалением токена если просрочен

--- api_gateway/src/gateway/repository.go
+++ api_gateway/src/gateway/repository.go
@@ -5,16 +5,27 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func getInfoAboutToken(writer http.ResponseWriter, request *http.Request) {
 	// Получение информации о токене
 	tokenFromHeader := request.Header.Get("Authorization")
 	writer.Header().Set("Content-Type", "application/json")
-	fmt.Println("Token from header", tokenFromHeader)
-	token := settings.GetTokenFromDatabase(tokenFromHeader)
-	if token == false {
-		writer.WriteHeader(http.StatusForbidden)
+	token, tokenErr := settings.GetTokenFromDatabase(tokenFromHeader)
+	if tokenErr != nil || token.Token == "" {
+		settings.BadToken(writer, request)
+		return
+	}
+	expireTime := token.Expire
+	currTime := time.Now().Round(0)
+	tokenExpired := expireTime.After(currTime)
+	if tokenExpired == false {
+		err := settings.DeleteTokenFromDatabase(token.Token)
+		if err != nil {
+			settings.DeleteError(writer, request)
+			return
+		}
 		settings.BadToken(writer, request)
 		return
 	}
