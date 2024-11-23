ef3a5cf37375f85a77b854c275df6754282bd5ff
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-11-23 13:47:20 +0700

    Обновил функцию получения токена из бд, добавил функцию удаления токена
    1. Функция получения токена теперь возвращает схему TokenApi и ошибку
    2. Cменил запросы orm на raw sql
    3. Добавил функцию удаления токена

--- api_gateway/src/settings/repository.go
+++ api_gateway/src/settings/repository.go
@@ -1,16 +1,32 @@
 package settings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func GetTokenFromDatabase(token string) interface{} {
-	// Получить объект из бд
+func GetTokenFromDatabase(token string) (TokenApi, error) {
+	// Получить токен из бд
 	database := GetDb()
-	var tokenModel Token
-	result := database.First(&tokenModel, "token = ?", token)
+	var tokenSchemas TokenApi
+	queryStr := fmt.Sprintf("SELECT * FROM Tokens WHERE Token = '%s'\n", token)
+	result := database.Raw(queryStr).Scan(&tokenSchemas)
 	if result.Error != nil {
 		fmt.Println("token get error", result.Error)
-		return false
+		return TokenApi{}, errors.New("token get error")
 	}
-	fmt.Println("token from db", tokenModel)
-	return tokenModel
+	return tokenSchemas, nil
+}
+
+func DeleteTokenFromDatabase(token string) error {
+	// удалить токен
+	database := GetDb()
+	var tokenSchemas TokenApi
+	queryStr := fmt.Sprintf("DELETE FROM Tokens WHERE Token = '%s'\n", token)
+	result := database.Raw(queryStr).Scan(&tokenSchemas)
+	if result.Error != nil {
+		fmt.Println("token delete error", result.Error)
+		return errors.New("token delete error")
+	}
+	return nil
 }
