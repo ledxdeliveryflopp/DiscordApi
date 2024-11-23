a6fbf418f37a76f426090489d9cf18acb3222065
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-11-23 13:44:19 +0700

    Добавил настройку максимального количества открытых соединений с бд

--- api_gateway/src/settings/database.go
+++ api_gateway/src/settings/database.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"time"
 )
 
 func GetDb() *gorm.DB {
@@ -12,5 +13,12 @@ func GetDb() *gorm.DB {
 	if err != nil {
 		fmt.Println("Database connect error", err)
 	}
+	connections, connectionErr := database.DB()
+	if connectionErr != nil {
+		fmt.Println("Database connection error", err)
+	}
+	connections.SetMaxIdleConns(10)
+	connections.SetMaxOpenConns(100)
+	connections.SetConnMaxLifetime(time.Hour)
 	return database
 }
