d9d4305f53fc88ea76dad4d27532a3ada673909d
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-12-06 00:31:13 +0700

    Добавлена инициализация роутера личных сообщений

---
+++ private_messages_microservice/chat/router.go
@@ -0,0 +1,10 @@
+package chat
+
+import "github.com/gorilla/mux"
+
+func SetUpPrivateRouter(mainRouter *mux.Router) {
+	chatRouter := mainRouter.PathPrefix("/private").Subrouter()
+	chatRouter.HandleFunc("/{chat_id}/messages", GetLastMessageFromChat).Methods("GET")
+	chatRouter.HandleFunc("/{chat_id}/info/", GetChatInfo).Methods("GET")
+	chatRouter.HandleFunc("/add-message/{chat_id}/", AddMessageInChat).Methods("POST")
+}
