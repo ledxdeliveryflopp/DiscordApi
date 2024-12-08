8cf244f80788dc3aa461744f3af6b11b92ca491e
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-12-08 16:08:00 +0700

    Изменил ключ токена

--- private_messages_microservice/chat/utils.go
+++ private_messages_microservice/chat/utils.go
@@ -9,7 +9,7 @@ type TokenSchemas struct {
 	Token string `json:"Token"`
 }
 
-var secretKey = []byte("1ca26a466e6327dfd6e51599fd2892e59ba1a2885ab3d9b09f48baaa3ca2251c")
+var secretKey = []byte("1a26a6e62dfe9fd2892e59b183d9b948baaa3ca2251c")
 
 func GetTokenPayload(headerToken string) (int, error) {
 	token, err := jwt.Parse(headerToken, func(token *jwt.Token) (interface{}, error) {
