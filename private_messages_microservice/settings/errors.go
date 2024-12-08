93fb1cd861e0a0cd76ecb9a53ec78e3b6d725e09
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-12-08 16:09:39 +0700

    Добавил функцию вызова ошибки при запросе

--- private_messages_microservice/settings/errors.go
+++ private_messages_microservice/settings/errors.go
@@ -1,5 +1,19 @@
 package settings
 
-type Error struct {
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type ErrorSchemas struct {
 	Detail string `json:"Detail"`
+}
+
+func RaiseError(writer http.ResponseWriter, errorDetail string, code int) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(code)
+	var errorSchemas ErrorSchemas
+	errorSchemas.Detail = errorDetail
+	json.NewEncoder(writer).Encode(errorSchemas)
+	return
 }
