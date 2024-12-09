22d548803c0de7235c3f44026904fc8a61bcec19
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-12-09 16:44:00 +0700

    Добавил структуру для создания приватного чата, добавил аватарку в структуру пользователя

--- private_messages_microservice/chat/struct.go
+++ private_messages_microservice/chat/struct.go
@@ -2,9 +2,9 @@ package chat
 
 // Users Структура пользователя
 type Users struct {
-	ID       int    `json:"ID" db:"id"`
-	Username string `json:"Username" db:"username"`
-	//AvatarUrl string `json:"AvatarUrl" db:"avatar_url"`
+	ID        int    `json:"ID" db:"id"`
+	Username  string `json:"Username" db:"username"`
+	AvatarUrl string `json:"AvatarUrl" db:"avatar_url"`
 }
 
 // PrivateChat Структура личного чата
@@ -27,9 +27,13 @@ type Message struct {
 	Text   string `json:"Text" validate:"required" db:"text"`
 	ChatId int    `json:"chat_id" validate:"required" db:"chat_id"`
 	Owner  Users  `json:"User"`
-	//OwnerId int `json:"OwnerId" db:"owner_id"`
 }
 
 type MessageCreate struct {
 	Text string `json:"Text" validate:"required,gte=0,lte=2000" db:"text"`
+}
+
+type PrivateChatCreate struct {
+	RecipientID int    `json:"recipient_id" validate:"required"`
+	Text        string `json:"text" validate:"lte=2000"`
 }
