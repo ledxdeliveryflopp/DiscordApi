bc00e6299fb0981052d124a3919690b665c8a75d
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-12-10 20:37:51 +0700

    Изменил миграции
    1. Убрал из миграции MR1 создание таблицы сообщений
    2. Создал файл миграции MR2 с создание таблицы сообщений
    3. Сделал поле "answer" в таблицу сообщений и привязал к "id" сообщений
    4. Установил ON DELETE на поля "answer" и "chat_id"

--- private_messages_microservice/migrations/MR1_init_chat_table.sql
+++ private_messages_microservice/migrations/MR1_init_chat_table.sql
@@ -5,12 +5,6 @@
 CREATE TABLE private_chat (id serial PRIMARY KEY, chat_starter_id integer, chat_recipient_id integer,
                            FOREIGN KEY (chat_starter_id) REFERENCES "users"(id), FOREIGN KEY (chat_recipient_id) REFERENCES "users"(id));
 
-CREATE TABLE message (id SERIAL PRIMARY KEY, text varchar, owner_id integer, chat_id integer,
-                      FOREIGN KEY (owner_id) REFERENCES "users"(id), FOREIGN KEY (chat_id) REFERENCES private_chat(id));
-
-
-
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
 DROP TABLE private_chat;
-DROP TABLE message;
