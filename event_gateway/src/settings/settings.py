918b2f0b2c40acd925386049c654d2ba138f3967
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-12-12 22:49:36 +0700

    Добавил микросервис для обслуживания "событий"
    События - авторизация через QR, новое сообщение в чате и тд

---
+++ event_gateway/src/settings/settings.py
@@ -0,0 +1,53 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class ApiSettings(BaseSettings):
+    """Настройки api"""
+    api_host: str
+    api_port: int
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+class DatabaseSettings(BaseSettings):
+    """Настройки БД"""
+    user: str
+    password: str
+    host: str
+    port: str
+    name: str
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+    @property
+    def get_full_db_path(self) -> str:
+        """Создание полного url для бд"""
+        return (f"postgresql+asyncpg://{self.user}:{self.password}@"
+                f"{self.host}:{self.port}/{self.name}")
+
+
+class TokenSettings(BaseSettings):
+    """Настройки токенов"""
+    secret: str
+    algorithm: str
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+class Settings(BaseSettings):
+    """Настройки"""
+    api_settings: ApiSettings
+    database_settings: DatabaseSettings
+    token_settings: TokenSettings
+
+
+@lru_cache()
+def init_settings() -> Settings:
+    """Инициализация настроек"""
+    return Settings(api_settings=ApiSettings(), database_settings=DatabaseSettings(), token_settings=TokenSettings())
+
+
+settings = init_settings()
+
