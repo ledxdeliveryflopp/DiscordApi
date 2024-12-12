30410e0272d0e060fcd05e77a64234eb281b5096
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-12-12 22:47:41 +0700

    Добавил пользователям поле qr_auth_token
    qr_auth_token, токен который нужен для авторизации через QR код.
    1. Содержит в себе email юзера
    2. Создается при регистрации
    3. Передается на эндпоинт который содержится в qr коде и отдает новый токен(что-то типо авторизации через телефон на пк)

--- authorization_microservice/src/registration/models.py
+++ authorization_microservice/src/registration/models.py
@@ -14,3 +14,4 @@ class UserModel(AbstractModel):
     status = Column(String(length=30), unique=False, nullable=True, comment="статус пользователя")
     country = Column(String(255), unique=False, nullable=False, comment="страна пользователя")
     password = Column(String(255), unique=False, nullable=True, comment="пароль пользователя")
+    qr_auth_token = Column(String, unique=False, nullable=False, comment="токен для авторизации через qr код")
