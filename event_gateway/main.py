def4e900f78c8b3d5854326630dae97b31abdf7e
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-12-13 16:59:27 +0700

    Добавил менеджер управления подключениями websocket
    Теперь авторизация через qr такая:
    1. у пользователя на сайте генерируется qr содержащий ссылку на вебсокет + идентификатор устройства
    2. Пользователь сканирует приложение на телефоне qr
    3. Приложение совершает запрос на эндпонит где генерируется зашифрованная информация о юзере и получает токен
    4. Приложение отправляет опкод "pending_ticket" с "encrypted_user_payload"(токен из 3 этапа)
    5. API проверяет токен и если все успешно отправляет его всем клиентам и разрывает соединение с статус кодом 1000
    6. Сайт после получения токена совершает запрос на эндпоинт авторизации передавая токен c идентификатором устройства  и получает jwt токен

--- event_gateway/main.py
+++ event_gateway/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+from loguru import logger
 from starlette.middleware.cors import CORSMiddleware
 
 from src.auth.router import auth_events_router
@@ -18,10 +19,13 @@ avents_gateway.add_middleware(
     allow_methods=["*"],
 )
 
+
 def run_app(host: str, port: int) -> None:
     """Запуск приложения"""
     uvicorn.run(app=avents_gateway, host=host, port=port, log_config="log.ini")
 
 
 if __name__ == '__main__':
+    logger.add("application.log", rotation="100 MB",
+               format="{time:DD-MM-YYYY at HH:mm:ss} | {level} | {message}")
     run_app(settings.api_settings.api_host, settings.api_settings.api_port)
---
+++ event_gateway/src/auth/sockets.py
@@ -0,0 +1,76 @@
+from loguru import logger
+from starlette.websockets import WebSocket
+
+
+class AuthConnectionManager:
+    """Класс менеджера управления группами авторизации"""
+    def __init__(self) -> None:
+        self.auth_hub_list: list[dict] = []
+        self.auth_hub_id_list: list[str] = []
+
+    async def create_auth_hub(self, client_id: str) -> None:
+        """Создание группы авторизации"""
+        if client_id in self.auth_hub_id_list:
+            pass
+        else:
+            hub = {"id": client_id, "clients": []}
+            self.auth_hub_list.append(hub)
+            self.auth_hub_id_list.append(client_id)
+
+    async def connect_to_auth_hub(self, websocket: WebSocket, client_id: str) -> None:
+        """Подключение к группе авторизации"""
+        for hub in self.auth_hub_list:
+            if hub.get("id") == client_id:
+                clients_list = hub.get("clients")
+                clients_list_len = len(clients_list)
+                if clients_list_len == 0:
+                    await websocket.accept()
+                    await websocket.send_json({"op": "wait mobile connect"})
+                    clients_list.append(websocket)
+                elif clients_list_len == 1:
+                    await websocket.accept()
+                    pc_client: WebSocket = clients_list[0]
+                    await pc_client.send_json({"op": "mobile connected"})
+                    clients_list.append(websocket)
+                else:
+                    await websocket.accept()
+                    await websocket.send_json({"op": "clients already in hub"})
+                    await websocket.close(1000, "full auth hub")
+            else:
+                await websocket.accept()
+                await websocket.send_json({"op": "bad hub id"})
+                await websocket.close(1000, "bad hab id")
+
+    async def heartbeat_ack(self, websocket: WebSocket, client_id: str) -> None:
+        """Ответ на heartbeat"""
+        for hub in self.auth_hub_list:
+            if hub.get("id") == client_id:
+                await websocket.send_json({"op": "heartbeat_ack"})
+
+    async def send_message_to_client(self, websocket: WebSocket, client_id: str, message: dict) -> None:
+        """Отправка сообщения клиенту"""
+        for hub in self.auth_hub_list:
+            if hub.get("id") == client_id:
+                await websocket.send_json(message)
+
+    async def broadcast(self, message: dict, client_id: str) -> None:
+        """Отправка сообщения всем клиентам группы"""
+        for hub in self.auth_hub_list:
+            if hub.get("id") == client_id:
+                clients_list = hub.get("clients")
+                for connection in clients_list:
+                    await connection.send_json(message)
+
+    async def success_close_all_connections(self, client_id: str) -> None:
+        """Успешно закрыть соединение с всеми клиентами в группе"""
+        for hub in self.auth_hub_list:
+            if hub.get("id") == client_id:
+                clients_list = hub.get("clients")
+                for connection in clients_list:
+                    await connection.close(1000, "success auth")
+                    self.auth_hub_list.remove(hub)
+                    self.auth_hub_id_list.remove(client_id)
+                    logger.debug(f"auth hub closed: {client_id}")
+
+
+socket_manager = AuthConnectionManager()
