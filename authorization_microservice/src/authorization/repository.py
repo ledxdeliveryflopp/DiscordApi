df0efddaf7b2b44ca45b142ddcc5eb317d5ed0ed
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2024-12-13 16:53:30 +0700

    Убрал функцию получения qr кода, изменил авторизацию через qr
    1. При авторизации(любой кроме Яндекса) необходимо передавать идентификатор устройства, например через fingerprintjs
    2. Сделал функцию генерации зашифрованной информации о юзере(токен который содержит хэшированный пароль)
    3. Изменил функцию авторизации по qr, теперь требуется идентификатор устройства и токен из 2 пункта

--- authorization_microservice/src/authorization/repository.py
+++ authorization_microservice/src/authorization/repository.py
@@ -42,6 +42,15 @@ class AuthorizationRepository(BaseService):
         expire = token_dict.get("expire")
         new_token_db = TokenModel(token=token, expire=expire)
         await self.save_object(new_token_db)
+        new_list = []
+        for i in check_user.clients_fingerprints:
+            if i == id:
+                pass
+            else:
+                new_list.append(i)
+        new_list.append(schemas.client_fingerprint)
+        check_user.clients_fingerprints = new_list
+        await self.save_user_object(check_user)
         return {"token": token, "type": "Bearer"}
 
     async def _repository_login_by_yandex_with_token(self, oauth_token: str) -> dict | HTTPException:
@@ -62,23 +71,42 @@ class AuthorizationRepository(BaseService):
         await self.save_object(new_token_db)
         return {"token": token, "type": "Bearer"}
 
-    @staticmethod
-    async def _repository_get_auth_qr_code() -> dict:
-        """Получить url к qr коду авторизации"""
-        qr_url = await s3_service.get_url_to_qr_file_in_s3("qr-code.png")
-        return {"detail": qr_url}
-
-    async def _repository_login_by_qr_code(self, auth_token: str) -> dict | HTTPException:
-        """Авторизация через qr код"""
-        token_payload = await get_token_payload(auth_token)
-        user_email = token_payload.get("user_email")
-        check_user = await self._find_user_by_email(user_email)
+    async def _repository_create_encrypted_user_payload(self, request: Request) -> dict | HTTPException:
+        """Создание токена с информацией о пользователе"""
+        header_token = request.headers.get("Authorization")
+        token_payload = await get_token_payload(header_token)
+        user_id = token_payload.get("user_id")
+        check_user = await self._find_user_by_id(user_id)
         if not check_user:
             raise UserDontExistException
+        user_email = token_payload.get("user_email")
+        auth_token = await create_auth_token(user_id, user_email, check_user.password)
+        return {"token": auth_token}
+
+    async def _repository_login_by_auth_token(self, request: Request, client_id: str) -> dict | HTTPException:
+        """Авторизация с помощью токена авторизации"""
+        header_token = request.headers.get("Authorization")
+        token_payload = await get_token_payload(header_token)
+        user_id = token_payload.get("user_id")
+        check_user = await self._find_user_by_id(user_id)
+        if not check_user:
+            raise UserDontExistException
+        user_hashed_password = token_payload.get("user_hashed_password")
+        if check_user.password != user_hashed_password:
+            raise BadPasswordException
         token_dict = await create_token(check_user.id, check_user.email)
         token = token_dict.get("token")
         expire = token_dict.get("expire")
         new_token_db = TokenModel(token=token, expire=expire)
         await self.save_object(new_token_db)
+        new_list = []
+        for i in check_user.clients_fingerprints:
+            if i == id:
+                pass
+            else:
+                new_list.append(i)
+        new_list.append(client_id)
+        check_user.clients_fingerprints = new_list
+        await self.save_user_object(check_user)
         return {"token": token, "type": "Bearer"}
 
--- authorization_microservice/src/authorization/router.py
+++ authorization_microservice/src/authorization/router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from starlette.requests import Request
 
 from src.authorization.schemas import LoginSchemas
 from src.authorization.service import AuthorizationService, init_authorization_service
@@ -18,14 +19,16 @@ async def router_login_by_yandex(oauth_token: str, service: AuthorizationService
     return await service.login_by_yandex(oauth_token)
 
 
-@auth_router.get("/get_qr_url/")
-async def router_create_auth_qr_code(service: AuthorizationService = Depends(init_authorization_service)):
+@auth_router.get("/encrypt_user_data/")
+async def router_create_encrypted_user_payload(request: Request,
+                                               service: AuthorizationService = Depends(init_authorization_service)):
     """Получение qr кода для авторизации"""
-    return await service.get_qr_code_url()
+    return await service.create_encrypted_user_payload(request)
 
 
 @auth_router.get("/qr_auth/")
-async def router_login_by_qr_code(auth_token: str, service: AuthorizationService = Depends(init_authorization_service)):
+async def router_login_by_qr_code(client_id: str, request: Request,
+                                  service: AuthorizationService = Depends(init_authorization_service)):
     """Роутер авторизации по qr коду"""
-    return await service.login_by_qr_code(auth_token)
+    return await service.login_by_auth_token(request, client_id)
 
