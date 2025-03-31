81aaa5e91ebee9574b419efd0f67ced95e46f79a
ledxdeliveryflopp <108229226+ledxdeliveryflopp@users.noreply.github.com>
2025-03-31 13:58:23 +0700

    добавил маркер для pytest, изменил команду для тестов

--- user_microservice/tests/user/test_user_router.py
+++ user_microservice/tests/user/test_user_router.py
@@ -1,5 +1,6 @@
 import datetime
 import allure
+import pytest
 from allure_commons.types import LabelType
 from pytest_mock import MockerFixture
 
@@ -7,6 +8,7 @@ from src.user.models import UserModel
 from user.const import MockerPathConst
 
 
+@pytest.mark.routers
 @allure.feature('Тестирование эндпоинтов микросервиса пользователей')
 class TestUserRouter:
     png_file = {"avatar_file": ("test.png", b"image data", "image/png")}
